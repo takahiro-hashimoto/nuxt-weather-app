e0b43cd4c9e6a113b083a46d194809e90010d4ce
takahiro-hashimoto <bridge.takahiro.1539@gmail.com>
2018-11-28 06:39:39 +0900

    first-commit

---
+++ pages/city/_name.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <h1>{{item.name}}の天気</h1>
+  <ul>
+    <li>天気：{{item.weather[0].main}}</li>
+    <li>温度：{{item.main.temp}}℃</li>
+    <li>湿度：{{item.main.humidity}}％</li>
+    <li>風速：{{item.wind.speed}}m</li>
+  </ul>
+  <nuxt-link to="/">トップに戻る</nuxt-link>
+</div>
+</template>
+
+<script>
+export default{
+  async asyncData({route, app}) {
+    const item = await app.$axios.$get(`https://api.openweathermap.org/data/2.5/weather?q=${route.params.name},jp&units=metric&appid=9c5838c2ce61b917f325ec05e5521ab9`)
+    return {
+      item
+    }
+  }
+}
+</script>
+
+<style media="screen">
+  .page-enter-active, .page-leave-active {
+    transition: opacity .5s;
+  }
+  .page-enter, .page-leave-to {
+    opacity: 0;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+<h1>日本全国の天気</h1>
+<ul>
+  <li><nuxt-link :to="`/city/sapporo`">札幌</nuxt-link></li>
+  <li><nuxt-link :to="`city/tokyo`">東京</nuxt-link></li>
+  <li><nuxt-link :to="`city/nagoya`">名古屋</nuxt-link></li>
+  <li><nuxt-link :to="`city/osaka`">大阪</nuxt-link></li>
+  <li><nuxt-link :to="`city/fukuoka`">福岡</nuxt-link></li>
+</ul>
+</div>
+</template>
+
+<style media="screen">
+  .page-enter-active, .page-leave-active {
+    transition: opacity .5s;
+  }
+  .page-enter, .page-leave-to {
+    opacity: 0;
+  }
+</style>
